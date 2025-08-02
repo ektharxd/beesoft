480a322e2f9aacd09a1145db2f1b58976e02aa4d
ektharxd <ekthar.xd@gmail.com>
2025-08-02 21:09:57 +0530

    Remove non-essential API files to stay under Vercel Hobby plan limit (12 functions)

--- api/test.js
+++
@@ -1,9 +0,0 @@
-// Simple test API endpoint
-module.exports = (req, res) => {
-  res.status(200).json({
-    message: 'Beesoft API is working!',
-    timestamp: new Date().toISOString(),
-    method: req.method,
-    url: req.url
-  });
-};
